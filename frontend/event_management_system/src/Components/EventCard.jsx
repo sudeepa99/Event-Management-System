f6f8bfe5b6f62e229ca941e534156384a4f871a1
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-17 20:40:02 +0530

    Event list page done

---
+++ frontend/event_management_system/src/Components/EventCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './EventCard.css';
+
+export default function EventCard(props) {
+  return (
+    <div className='event_cards'>
+    
+        <Card style={{ width: 'auto' }}>
+        <Card.Body>
+        <Card.Title>{props.event_id}</Card.Title>
+        <Card.Title>{props.name}</Card.Title>
+        <div className='button_section'>
+        <Button className='more_details' >More Details</Button>
+        <Button className='update'>Update Event</Button>
+        <Button className='delete'>Delete Event</Button>
+        </div>
+        </Card.Body>
+        </Card>
+
+      
+    </div>
+  )
+}
--- frontend/event_management_system/src/pages/Events.jsx
+++ frontend/event_management_system/src/pages/Events.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import NavigationBar from '../Components/NavigationBar'
+import './Events.css';
+import Event_Card from '../Components/EventCard';
+import EventCard from '../Components/EventCard';
 export default function Events() {
   return (
     <div>
-
+        <NavigationBar/>
+        <h1 className='event_head'>Event List</h1>
+        <EventCard/>
         
       
     </div>

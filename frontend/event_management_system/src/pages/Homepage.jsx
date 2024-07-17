ac081ceeecb5c6100bcc3e7affa43be3595d674c
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-17 12:11:11 +0530

    navigation bar added

--- frontend/event_management_system/src/pages/Homepage.jsx
+++ frontend/event_management_system/src/pages/Homepage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavigationBar from '../Components/NavigationBar'
 
 export default function Homepage() {
   return (
     <div>
+        <NavigationBar/>
       
     </div>
   )

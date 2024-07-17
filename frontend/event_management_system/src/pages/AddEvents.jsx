b236d4b7a87cae1c6322a5b74f3f4349741e59d0
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-18 01:28:31 +0530

    form completed

--- frontend/event_management_system/src/pages/AddEvents.jsx
+++ frontend/event_management_system/src/pages/AddEvents.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavigationBar from '../Components/NavigationBar'
+import Form from '../Components/Form'
 
 export default function AddEvents() {
   return (
     <div>
+        <NavigationBar/>
+        <Form/>
+
       
     </div>
   )
--- frontend/event_management_system/src/pages/Events.jsx
+++ frontend/event_management_system/src/pages/Events.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import NavigationBar from '../Components/NavigationBar'
 import './Events.css';
-import Event_Card from '../Components/EventCard';
 import EventCard from '../Components/EventCard';
 export default function Events() {
   return (
